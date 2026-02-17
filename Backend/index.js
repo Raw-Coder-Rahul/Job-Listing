fc15f842ddec4c83664deef7bfb49fa5fa66a5ee
Raw-Coder-Rahul <rb3335167@gmail.com>
2026-02-18 00:37:46 +0530

    user

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
 
 dotenv.config();
 const app = express();
@@ -28,6 +29,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
   console.log(`If check click http://localhost:${PORT}`);
 });
