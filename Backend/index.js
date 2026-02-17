d8a16c68d311d3ffe4b822b37860f71029792bcf
Raw-Coder-Rahul <rb3335167@gmail.com>
2026-02-17 11:09:02 +0530

    backend start

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: ["http://localhost:5474"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+// routes
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the API!",
+    timestamp: new Date().toISOString(),
+    success: true,
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`If check click http://localhost:${PORT}`);
+});
