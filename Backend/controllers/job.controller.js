bc9afefe5d73a2638696e08a461c2e71a2feb3ee
Raw-Coder-Rahul <rb3335167@gmail.com>
2026-02-23 00:48:27 +0530

    company add Backend

--- Backend/controllers/job.controller.js
+++ Backend/controllers/job.controller.js
@@ -1,29 +1,31 @@
 // Admin Jobs
-import { Job } from "../models/job.model";
+import { Job } from "../models/job.model.js";
 
 export const postJob = async (req, res) => {
     try {
         const { title, description, requirements, salary, location, jobType, position, companyId, experience } = req.body;
         const userId = req.id;
-        if (!title || !description || !requirements || !salary || !companyId || !experience || !jobType || !position || location) {
+
+        if (!title || !description || !requirements || !salary || !companyId || !experience || !jobType || !position || !location) {
             return res.status(400).json({ message: "Please fill all the fields", status: false });
         }
 
-        const job = await createImageBitmap({
+        const job = new Job({
             title,
             description,
             requirements: requirements.split(","),
-            salary: Number(salary),
+            salary,
             location,
             jobType,
             position,
             company: companyId,
-            experienceLevel: experience,
+            experience,
             created_by: userId,
-        })
-        return res
-            .status(201)
-            .json({ message: "Job posted successfully", status: true, job });
+        });
+
+        await job.save();
+
+        return res.status(201).json({ message: "Job posted successfully", status: true, job });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: "Server error", status: false });
@@ -31,27 +33,29 @@ export const postJob = async (req, res) => {
 };
 
 export const getAllJobs = async (req, res) => {
-    try {
-        const keyword = req.query.keyword || "";
-        const query = {
-            $or: [
-                { title : { $regex: keyword, $options: "i" }},
-                { description : { $regex: keyword, $options: "i" }},
-                { requirements : { $regex: keyword, $options: "i" }},
-                { location : { $regex: keyword, $options: "i" }},
-                { jobType : { $regex: keyword, $options: "i" }},
-                { position : { $regex: keyword, $options: "i" }},
-            ],
-        };
-        const jobs = await Job.find(query);
-        if (!jobs) {
-            return res.status(404).json({ message: "No jobs found", status: false });
-        }
-        return res.status(200).json({ jobs, status: true });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Server Error", status: false });
+  try {
+    const keyword = req.query.keyword || "";
+    const query = {
+      $or: [
+        { title: { $regex: keyword, $options: "i" }},
+        { description: { $regex: keyword, $options: "i" }},
+      ],
+    };
+
+    const jobs = await Job.find(query)
+        .populate("company", "name description userId createdAt updatedAt")
+        .sort({ createdAt: -1 });
+
+
+    if (jobs.length === 0) {
+      return res.status(404).json({ message: "No jobs found", status: false });
     }
+
+    return res.status(200).json({ jobs, status: true });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server Error", status: false });
+  }
 };
 
 // Users
@@ -70,7 +74,7 @@ export const getJobById = async (req, res) => {
 }
 
 // Admin Job Created
-export const getAdminJob = async ( req, res ) => {
+export const getAdminJobs = async ( req, res ) => {
     try {
         const adminId = req.id;
         const jobs = await Job.find({ created_by: adminId });
@@ -82,4 +86,4 @@ export const getAdminJob = async ( req, res ) => {
         console.error(error);
         return res.status(500).json({ message: "Server Error", status: false });
     }
-}
+};
